4804ce82725072310ab332ed50efbce72ff675c3
Samarth <43917503+samarthgupta0309@users.noreply.github.com>
2021-10-01 23:36:15 +0530

    Add files via upload

--- data_preprocessing/preprocessing.py
+++ data_preprocessing/preprocessing.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 class Preprocessor:
     '''
@@ -55,16 +57,54 @@ class Preprocessor:
         '''
         self.logger_object.log(self.file_object, 'Started : Preprocessor -> scale_numerical_col')
         self.data = data
-        self.numerical_data = self.data.drop(["potential_issue","deck_risk","ppap_risk","stop_auto_buy","rev_stop"],axis=1)
+        self.numerical_data = self.data.drop(["potential_issue","deck_risk","ppap_risk","stop_auto_buy","rev_stop","went_on_backorder"],axis=1)
         try:
             self.scaler = StandardScaler()
             self.scaled_data = self.scaler.fit_transform(self.numerical_data)
-            self.scaled_df = pd.DataFrame(data = self.scaled_data, columns=self.numerical_data.columns, index = data.self)
+            self.scaled_df = pd.DataFrame(data = self.scaled_data, columns=self.numerical_data.columns, index = self.data.index)
             self.data.drop(columns = self.scaled_df.columns, inplace=True)
             self.data = pd.concat([self.scaled_df, self.data], axis=1)
+            self.data.to_csv('data_preprocessing/scaled_data.csv')
             self.logger_object.log(self.file_object, 'Success : Scaled all the numerical values ')
             return self.data
         except Exception as e:
             self.logger_object.log(self.file_object, 'Failure : Scaled all the numerical values '+ str(e))
             raise Exception()
+
+    def check_null(self, data):
+        self.logger_object.log(self.file_object, 'Started : Preprocessor -> check_null')
+        self.null_present = False
+        try:
+            self.null_count = data.isna().sum()
+            self.null_column = []
+            for col, val in zip(list(data.columns), self.null_count):
+                if val > 0:
+                    self.null_present = True
+                    self.null_column.append(col)
+            if(self.null_present):
+                self.logger_object.log(self.file_object, "Null columns : ")
+                for col in self.null_column:
+                    # print(f"{col} ")
+                    self.logger_object.log(self.file_object," "+str(col))
+            self.logger_object.log(self.file_object, "Success : check_null completed")
+            return self.null_column
+        except Exception as e:
+            self.logger_object.log(self.file_object, "Failure : check_null "+ str(e))
+
+    def split(self, X, y):
+        '''
+        Split train test in ratio X_train, X_test, y_train, y_test
+        train : test ratio   70:30
+        param : 
+            - X - data excluding null value
+            - y - target
+        '''
+        self.logger_object.log(self.file_object, "Started : Preprocessor -> split")
+        try:
+            self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+            self.logger_object.log(self.file_object, "Success : Split Completed")
+            return self.X_train, self.X_test, self.y_train, self.y_test
+        except Exception as e:
+            self.logger_object.log(self.file_object, "Failure : split uncompleted")
+            raise Exception() 
 
--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 from datetime import datetime
-import os
+import joblib
 import pandas as pd
 from app_logging.logging import App_Logger
 from data_loading.data_loader import Data_load
 from data_preprocessing.preprocessing import Preprocessor
+from model_find.model_dispatcher import Make_model
 
 if __name__ == "__main__":
     file_object = open("Logs/log.txt", 'a+')
     log_writer = App_Logger()
     try:
         data_get = Data_load(file_object, log_writer)
+        log_writer.log(file_object, "\n")
         data = data_get.get_data()
         preprocess = Preprocessor(file_object, log_writer)
         data = preprocess.drop_column(data, ["Index_Product", "sku"])
         data = preprocess.encodeCatFeature(data)
-        print(data.head(10))
-        log_writer.log(file_object, 'Success in the task')
+        null_col = preprocess.check_null(data)
+        if len(null_col) != 0:
+            data.dropna(inplace=True)
+        data = preprocess.scale_numerical_col(data)
+        X = data.drop(['went_on_backorder'], axis=1)
+        y = data['went_on_backorder']
+        X_train, X_test, y_train, y_test = preprocess.split(X, y)
+        make_model = Make_model(file_object, log_writer)
+        make_model.best_model(X_train, y_train)
+        load_model = joblib.load(open('model_find/model.sav', 'rb'))
+        result = load_model.score(X_test, y_test)
+        log_writer.log(file_object, "Result of model "+str(result))
+        log_writer.log(file_object, "Success in task !!")
         file_object.close()
     except Exception as e:
         log_writer.log(file_object, "Failure "+ str(e))
